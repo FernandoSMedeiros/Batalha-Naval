eb8ead057a230e1c4640ee9baf00061847bdd382
Fernando Silva Medeiros <fernando.medeiros.ti@gmail.com>
2017-09-30 21:32:26 -0300

    implementacao do toString em tabuleiro

---
+++ Tabuleiro.lua
@@ -0,0 +1,65 @@
+local tabuleiro = {{}, {}, {}, {}, {}, {}, {}, {}, {}, {}}
+
+function tabuleiro:novo()
+  
+  local novo = {}
+  setmetatable(novo, {__index = tabuleiro})
+  self:vazio()
+  
+  return novo
+  
+end
+
+function tabuleiro:vazio()
+    for i=1, 10 do
+      for j=1, 10 do
+          self[i][j] = "*"
+        end
+      end
+  end
+  
+  
+  function tabuleiro:inserirNavio(navio, posicao)
+    
+    
+    
+  end
+  
+  function tabuleiro:toString()
+ 
+    local string = ""
+    local coluna = ""
+    
+    for i = 1, 10 do
+     
+        if i == 1 then
+          coluna = coluna ..  "  " .. i .. " "      
+        else
+          coluna = coluna .. i .. " "
+        end  
+     
+     
+      for j = 1, 10 do
+        
+        if j == 1 then
+          string = string .. (string.char(64 + i)) .. " "        
+        end
+        
+        string = string .. self[i][j] .. " "
+        
+        if j == 10 then
+          string = string .. "\n"
+        end
+        end
+    end
+    print(coluna)
+    print(string)
+  end
+  
+ 
+  tabuleiro:novo()
+  tabuleiro:toString()
+  
+  tabuleiro:novo()
+  tabuleiro:toString()
+  
